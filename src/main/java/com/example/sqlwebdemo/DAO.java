bff72a9a76fb1bc8058e2d0a03dad7a1cb870c59
Peter <peter.pilestedt@gmail.com>
2021-04-15 14:56:18 +0200

    fixed old dao constructor

--- src/main/java/com/example/sqlwebdemo/DAO.java
+++ src/main/java/com/example/sqlwebdemo/DAO.java
@@ -10,12 +10,7 @@ public class DAO {
     private List<Book> booklist = new ArrayList<>();
 
     public DAO(){
-        booklist.add(new Book(1,"titel1","author1"));
-        booklist.add(new Book(2,"titel2","author2"));
-        booklist.add(new Book(3,"titel3","author3"));
-        booklist.add(new Book(4,"titel4","author4"));
-        booklist.add(new Book(5,"titel5","author5"));
-        booklist.add(new Book(6,"titel6","author6"));
+
     }
 
     public List<Book> getBooklist(){
